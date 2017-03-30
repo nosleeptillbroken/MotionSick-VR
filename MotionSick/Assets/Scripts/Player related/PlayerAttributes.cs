450e6103af8f75168b7a56022be7d1821891518b
DESKTOP-RIDGAOB\Robert <robwhyte12@gmail.com>
2017-03-30 02:00:21 -0400

    Player is now able to be killed Player now has a health pool There is now a Game Over screen after Player death There is now a Game Over menu prefab There is now a Player Character Prefab there is now a Rewire Input Handler Prefab

---
+++ MotionSick/Assets/Scripts/Player related/PlayerAttributes.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttributes : MonoBehaviour
+{
+
+    // How many hits you can take before dying
+    public int playerHealth;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Decrement Health Variable
+        playerHealth--;
+        // Just for testing
+        Debug.Log("boop");
+        // If the Player is Dead
+        if (playerHealth <= 0)
+        {
+            // If the collision is with an enemy
+            if (other.tag == "enemy")
+            {
+                // Begin the Process of killing the player
+                GetComponent<PlayerDeath>().OnDeath();
+            }
+        }
+    }
+
+}
