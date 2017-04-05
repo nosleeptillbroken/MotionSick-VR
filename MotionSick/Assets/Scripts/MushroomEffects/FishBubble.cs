aa79ad96cbb488e91696ff87440c95266aef19dd
jthra037 <its_thrasher@live.ca>
2017-04-04 23:55:30 -0400

    Fix Jacob working scene

--- MotionSick/Assets/Scripts/MushroomEffects/FishBubble.cs
+++ MotionSick/Assets/Scripts/MushroomEffects/FishBubble.cs
@@ -1,16 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityStandardAssets.ImageEffects;
 using UnityEngine;
 
-public class FishBubble : MonoBehaviour {
+public class FishBubble : Effect {
+    Fisheye fisheye;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    float max = 0.15f;
+    float t = 0;
+    float previous;
+
+    bool fadingIn;
+
+    public override void turnOn()
+    {
+        fisheye = Cam.GetComponent<Fisheye>();
+        killTime = 5f;
+        fadingIn = true;
+    }
+
+    public override void run(float intensity)
+    {
+        if (On)
+        {
+          //  fisheye.intensity = (max * intensity) * Mathf.Sin(toRad(Time.time - time) / loopTime) + (max * intensity) + 0.15f;
+            //previous = fisheye.intensity;
+        }
+        else if (turningOff && previous > 0)
+        {
+            t += (1 / killTime) * Time.deltaTime;
+            //fisheye.intensity = Mathf.Lerp(previous, 0, t);
+        }
+        else if (turningOff)
+        {
+            turningOff = false;
+            t = 0;
+        }
+    }
 }
