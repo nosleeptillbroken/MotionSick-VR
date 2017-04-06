1371982b5e38722a15b11f4ef5d64426bff17064
jthra037 <its_thrasher@live.ca>
2017-04-06 16:05:03 -0400

    Add destructible end table and bookshelf

---
+++ MotionSick/Assets/Scripts/KnockMeDown.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+public class KnockMeDown : MonoBehaviour {
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        rb.isKinematic = false;
+    }
+}
