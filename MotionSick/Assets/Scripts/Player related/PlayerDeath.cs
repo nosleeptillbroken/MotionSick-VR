4c33dee03fcc2834c56198ab0f106ec8033202f0
DESKTOP-RIDGAOB\Robert <robwhyte12@gmail.com>
2017-04-07 12:30:01 -0400

    Added Main Menu prefab Added Victory screen Prefab Added menu functions script Added Player Victory Script

--- MotionSick/Assets/Scripts/Player related/PlayerDeath.cs
+++ MotionSick/Assets/Scripts/Player related/PlayerDeath.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class PlayerDeath : MonoBehaviour
 {
@@ -9,17 +8,10 @@ public class PlayerDeath : MonoBehaviour
     //This Script needs to be attached to a Player to work
     private void Start()
     {
-        Scene menuScene = SceneManager.GetActiveScene();
-        Scene currentScene = SceneManager.GetActiveScene();
     }
 
     //Whatever you want as your Game Over Menu
     public GameObject gameOverMenu;
-    // Menu Scene, currentyly set to be the active scene
-    //public string menuScene = "Rob";
-    
-    // Menu Scene, currentyly set to be the active scene
-    //public string currentScene = "Rob";
     
     // Call this when health equals 0
     // It disables the PlayerController
@@ -30,29 +22,10 @@ public class PlayerDeath : MonoBehaviour
         GetComponent<PlayerController>().enabled = false;
         // Create the Game Over menu
         Instantiate(gameOverMenu);
+        Cursor.visible = true;
     }
 
     // Replay Menu Button
     //There are some issues with reloading the scene and Rewired
-    public void Replay()
-    {
-        // This will be the name of whichever scene we are playing in
-        //SceneManager.LoadScene(currentScene.name);
-        Debug.Log("Reloaded");
-    }
-
-    //Return To Menu Button
-    // Assuming main menu is in a different scene.
-    public void ReturnToMenu()
-    {
-        // This will be the name of the scene where we keep the menu
-        //SceneManager.LoadScene(menuScene.name);
-        Debug.Log("Main Menu Loaded");
-    }
-
-    //Quit Button
-    public void QuitButton()
-    {
-        Application.Quit();
-    }
+    
 }
