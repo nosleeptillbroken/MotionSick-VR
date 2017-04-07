9744e57c4679b82bf51fe286f5669d4ce32709f6
Mark Miller <mark-miller04@outlook.com>
2017-04-07 12:26:52 -0400

    Added some objects, and a few scripts for events

---
+++ MotionSick/Assets/Scripts/Object Scripts/Machine.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Machine : MonoBehaviour
+{
+    private bool activated = false;
+    public AudioSource noise;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.gameObject.CompareTag("Player") && activated == false)
+        {
+            activated = true;
+            //noise.Play();
+            Debug.Log("RRRRRRRRRMMMMMMMM");
+        }
+    }
+
+    public void interact()
+    {
+        //noise.Stop();
+        Debug.Log("shut that racket off");
+    }
+}
