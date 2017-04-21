01abad62318a477966f26344484c14e17caeaa4f
jthra037 <its_thrasher@live.ca>
2017-04-20 22:25:24 -0400

    Remove Player only collision stipulation Add collision layer for pickup objects

--- MotionSick/Assets/Scripts/KnockMeDown.cs
+++ MotionSick/Assets/Scripts/KnockMeDown.cs
@@ -15,10 +15,7 @@ public class KnockMeDown : MonoBehaviour {
 
     void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.CompareTag("Player"))
-        { 
-            transform.parent.SendMessage("OnCollisionEnter", other, SendMessageOptions.DontRequireReceiver);
-            rb.isKinematic = false;
-        }
+        transform.parent.SendMessage("OnCollisionEnter", other, SendMessageOptions.DontRequireReceiver);
+        rb.isKinematic = false;
     }
 }
