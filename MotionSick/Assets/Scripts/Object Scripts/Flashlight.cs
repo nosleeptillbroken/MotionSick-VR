1cc6b854e80029180a9da7c4b9f8187aa5ecff7d
jthra037 <its_thrasher@live.ca>
2017-04-20 23:21:30 -0400

    Add working flashlight

---
+++ MotionSick/Assets/Scripts/Object Scripts/Flashlight.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour {
+
+    private bool running = false;
+
+    private bool dropped = true;
+
+    [SerializeField]
+    private Light lightSource;
+
+	// Use this for initialization
+	void Start () {
+        lightSource.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.parent != null && dropped)
+        {
+            dropped = false;
+
+            transform.localRotation = Quaternion.identity;
+            Vector3 newAngle = transform.localEulerAngles;
+            newAngle.x = 90;
+            transform.localRotation = Quaternion.Euler(newAngle);
+
+            Vector3 newPosition = transform.localPosition;
+            newPosition.x = 0;
+            newPosition.y = 0;
+            transform.localPosition = newPosition;
+
+            if(!running)
+            {
+                running = true;
+                lightSource.enabled = true;
+            }
+        }
+
+        if (transform.parent == null && !dropped)
+            dropped = true;
+	}
+}
